e9b10a7b1aa7ccb923c5db291fcee7926256d153
Wojciech Paradziński <paradzinskiwojciech@gmail.com>
2025-10-18 20:58:08 +0200

    Add product button, product information form

--- EcoAssistant/app/src/main/java/com/example/myapplication/AddProductsFragment.java
+++ EcoAssistant/app/src/main/java/com/example/myapplication/AddProductsFragment.java
@@ -3,21 +3,17 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ProductsFragment#newInstance} factory method to
+ * Use the {@link AddProductsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProductsFragment extends Fragment {
+public class AddProductsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,7 +24,7 @@ public class ProductsFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ProductsFragment() {
+    public AddProductsFragment() {
         // Required empty public constructor
     }
 
@@ -38,11 +34,11 @@ public class ProductsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ProductsFragment.
+     * @return A new instance of fragment AddProductsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ProductsFragment newInstance(String param1, String param2) {
-        ProductsFragment fragment = new ProductsFragment();
+    public static AddProductsFragment newInstance(String param1, String param2) {
+        AddProductsFragment fragment = new AddProductsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -63,20 +59,6 @@ public class ProductsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_products, container, false);
-
-        // Find and set click listener for the add product button
-        FloatingActionButton addProductButton = view.findViewById(R.id.add_product_button);
-        addProductButton.setOnClickListener(v -> navigateToAddProducts());
-
-        return view;
-    }
-
-    private void navigateToAddProducts() {
-        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout, new AddProductsFragment());
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+        return inflater.inflate(R.layout.fragment_add_products, container, false);
     }
 }
