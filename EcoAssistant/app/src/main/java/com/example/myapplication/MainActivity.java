be23f1d95fc91f61b5963e969964859c61f39d97
Wojciech Paradziński <paradzinskiwojciech@gmail.com>
2025-10-18 17:05:41 +0200

    4 icon bottom nav bar

---
+++ EcoAssistant/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        replaceFragment(new ProductsFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.products) {
+                replaceFragment(new ProductsFragment());
+            }
+            else if (item.getItemId() == R.id.deposit) {
+                replaceFragment(new DepositFragment());
+            }
+            else if (item.getItemId() == R.id.reports) {
+                replaceFragment(new ReportsFragment());
+            }
+            else if (item.getItemId() == R.id.settings) {
+                replaceFragment(new SettingsFragment());
+            }
+            return true;
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
